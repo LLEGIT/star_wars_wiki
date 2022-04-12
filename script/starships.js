ff7fc8d00329cf5477851627458d08451760c4c8
LLEGIT <theogillet.bordeaux@outlook.com>
2022-04-12 10:46:11 +0200

    Vehicles & starships well displayed all infos displayed

--- script/starships.js
+++ script/starships.js
@@ -1,29 +1,30 @@
 let page = 1
 
-document.addEventListener("DOMContentLoaded", showSpecies(page))
+document.addEventListener("DOMContentLoaded", showStarships(page))
 
-function showSpecies(page) {
+function showStarships(page) {
     let page_nb = document.querySelector("#page-nb")
     page_nb.textContent = `Page ${page}`
     let content_container = document.querySelector(".content-container")
     content_container.innerHTML = ""
     // Fetching every characters
-    fetch(`https://swapi.dev/api/species/?page=${page}`)
+    fetch(`https://swapi.dev/api/starships/?page=${page}`)
     .then((resp) => resp.json())
     .then(function (data) {
-        let species = data.results
-        species.forEach(specy => {
+        let starships = data.results
+        starships.forEach(starship => {
                 content_container.innerHTML += `<div class='col-lg-12 col-sm-12'><div class='card'>
                 <img class="card-img-top" src="https://via.placeholder.com/300" alt="Card image cap">
                 <div class='card-body'>
-                <h5 class='card-title'>${specy.name}</h5>
-                <p class='card-text'>Classification: ${specy.classification}</p>
-                <p class='card-text'>Designation: ${specy.designation}</p>
-                <p class='card-text'>Average height: ${specy.average_height} cm</p>
-                <p class='card-text'>Average lifespan: ${specy.average_lifespan} years</p>
-                <p class='card-text'>Skin colors: ${specy.skin_colors}</p>
-                <p class='card-text'>Hair colors: ${specy.hair_colors}</p>
-                <p class='card-text'>Eye colors: ${specy.eye_colors}</p>
+                <h5 class='card-title'>${starship.name}</h5>
+                <p class='card-text'>Model: ${starship.model}</p>
+                <p class='card-text'>Class: ${starship.starship_class}</p>
+                <p class='card-text'>Manufacturer: ${starship.manufacturer}</p>
+                <p class='card-text'>Cost in credits: ${starship.cost_in_credits} credits</p>
+                <p class='card-text'>Length: ${starship.length} m</p>
+                <p class='card-text'>Max atmosphering speed: ${starship.max_atmosphering_speed}</p>
+                <p class='card-text'>Crew: ${starship.crew} persons</p>
+                <p class='card-text'>Passengers: ${starship.passengers} passengers</p>
                 </div></div></div>`
         })
     })
@@ -35,7 +36,7 @@ function showSpecies(page) {
     document.querySelector("#next-btn").addEventListener("click", function() {
         if (page < 4) {
             page += 1
-            showSpecies(page)
+            showStarships(page)
         }
         else {
             alert("You've reached last page")
@@ -45,7 +46,7 @@ function showSpecies(page) {
     document.querySelector("#prev-btn").addEventListener("click", function() {
         if (page > 1) {
             page -= 1
-            showSpecies(page)
+            showStarships(page)
         }
         else {
             alert("You're on page 1 !")
--- script/vehicles.js
+++ script/vehicles.js
@@ -1,29 +1,30 @@
 let page = 1
 
-document.addEventListener("DOMContentLoaded", showSpecies(page))
+document.addEventListener("DOMContentLoaded", showVehicles(page))
 
-function showSpecies(page) {
+function showVehicles(page) {
     let page_nb = document.querySelector("#page-nb")
     page_nb.textContent = `Page ${page}`
     let content_container = document.querySelector(".content-container")
     content_container.innerHTML = ""
     // Fetching every characters
-    fetch(`https://swapi.dev/api/species/?page=${page}`)
+    fetch(`https://swapi.dev/api/vehicles/?page=${page}`)
     .then((resp) => resp.json())
     .then(function (data) {
-        let species = data.results
-        species.forEach(specy => {
+        let vehicles = data.results
+        vehicles.forEach(vehicles => {
                 content_container.innerHTML += `<div class='col-lg-12 col-sm-12'><div class='card'>
                 <img class="card-img-top" src="https://via.placeholder.com/300" alt="Card image cap">
                 <div class='card-body'>
-                <h5 class='card-title'>${specy.name}</h5>
-                <p class='card-text'>Classification: ${specy.classification}</p>
-                <p class='card-text'>Designation: ${specy.designation}</p>
-                <p class='card-text'>Average height: ${specy.average_height} cm</p>
-                <p class='card-text'>Average lifespan: ${specy.average_lifespan} years</p>
-                <p class='card-text'>Skin colors: ${specy.skin_colors}</p>
-                <p class='card-text'>Hair colors: ${specy.hair_colors}</p>
-                <p class='card-text'>Eye colors: ${specy.eye_colors}</p>
+                <h5 class='card-title'>${vehicles.name}</h5>
+                <p class='card-text'>Model: ${vehicles.model}</p>
+                <p class='card-text'>Class: ${vehicles.vehicle_class}</p>
+                <p class='card-text'>Manufacturer: ${vehicles.manufacturer}</p>
+                <p class='card-text'>Cost in credits: ${vehicles.cost_in_credits} credits</p>
+                <p class='card-text'>Length: ${vehicles.length} m</p>
+                <p class='card-text'>Max atmosphering speed: ${vehicles.max_atmosphering_speed}</p>
+                <p class='card-text'>Crew: ${vehicles.crew} persons</p>
+                <p class='card-text'>Passengers: ${vehicles.passengers} passengers</p>
                 </div></div></div>`
         })
     })
@@ -35,7 +36,7 @@ function showSpecies(page) {
     document.querySelector("#next-btn").addEventListener("click", function() {
         if (page < 4) {
             page += 1
-            showSpecies(page)
+            showVehicles(page)
         }
         else {
             alert("You've reached last page")
@@ -45,7 +46,7 @@ function showSpecies(page) {
     document.querySelector("#prev-btn").addEventListener("click", function() {
         if (page > 1) {
             page -= 1
-            showSpecies(page)
+            showVehicles(page)
         }
         else {
             alert("You're on page 1 !")
