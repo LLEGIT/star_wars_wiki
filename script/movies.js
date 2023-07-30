c56b5002e5c3f2536896fe9383eceeffe064d29c
LLEGIT <theogillet.developpement@gmail.com>
2023-07-30 11:05:13 +0200

    refactor: added loader and optimized function

--- script/movies.js
+++ script/movies.js
@@ -1,49 +1,49 @@
 document.addEventListener("DOMContentLoaded", showMovies())
 
 function showMovies() {
-    let content_container = document.querySelector(".content-container")
+    let content_container = document.querySelector(".content-container");
+    let moviesPoster = [
+        "https://i.ibb.co/vsbyHfC/ep-I-poster.jpg",
+        "https://i.ibb.co/ZNM02rc/ep-II-poster.jpg",
+        "https://i.ibb.co/hsx4PwW/ep-III-poster.jpg",
+        "https://i.ibb.co/TqmKZpx/ep-IV-poster.jpg",
+        "https://static.wikia.nocookie.net/starwars/images/e/e4/Empire_strikes_back_old.jpg/revision/latest/scale-to-width-down/499?cb=20161114072554",
+        "https://i.ibb.co/6byn7R9/ep-VI-poster.jpg"
+    ];
+
+    // While movies not fetched
+    content_container.innerHTML = "<div class='lds-roller'><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>";
+
     // Getting films JSON
     fetch("https://swapi.dev/api/films")
-        .then((resp) => resp.json())
+        .then((response) => response.json())
         .then(function (data) {
+            // Empty the view
+            content_container.innerHTML = "";
             let movies = data.results
             let movie_image = ""
-            movies.forEach(movie => {
+            movies.forEach((movie, index) => {
                 // Defining movie poser
-                if (movie.episode_id === 1) {
-                    movie_image = "https://i.ibb.co/vsbyHfC/ep-I-poster.jpg"
-                }
-                if (movie.episode_id === 2) {
-                    movie_image = "https://i.ibb.co/ZNM02rc/ep-II-poster.jpg"
-                }
-                if (movie.episode_id === 3) {
-                    movie_image = "https://i.ibb.co/hsx4PwW/ep-III-poster.jpg"
-                }
-                if (movie.episode_id === 4) {
-                    movie_image = "https://i.ibb.co/TqmKZpx/ep-IV-poster.jpg"
-                }
-                if (movie.episode_id === 5) {
-                    movie_image = "https://static.wikia.nocookie.net/starwars/images/e/e4/Empire_strikes_back_old.jpg/revision/latest/scale-to-width-down/499?cb=20161114072554"
-                }
-                if (movie.episode_id === 6) {
-                    movie_image = "https://i.ibb.co/6byn7R9/ep-VI-poster.jpg"
-                }
-                content_container.innerHTML += `<div class='card'>
-                <div class='col-lg-3 col-sm-3 col-xs-6'>
-<img class="card-img-top" src="${movie_image}" alt="Card image cap">
-                </div>
-                <div class = 'col-lg-9 col-sm-9 col-xs-6'>
-<div class='card-body'>
-<h5 class='card-title'>${movie.title}</h5>
-<p class='card-text'>${movie.opening_crawl}</p>
-<p class='card-text'>Director: ${movie.director}</p>
-<p class='card-text'>Producer ${movie.producer}</p>
-<p class='card-text'>Release date: ${movie.release_date}</p>
-</div>
-</div></div>`
+                movie_image = moviesPoster[index];
+
+                content_container.innerHTML += 
+                `<div class='card'>
+                    <div class='col-lg-3 col-sm-3 col-xs-6'>
+                        <img class="card-img-top" src="${movie_image}" alt="Card image cap">
+                    </div>
+                    <div class = 'col-lg-9 col-sm-9 col-xs-6'>
+                        <div class='card-body'>
+                            <h5 class='card-title'>${movie.title}</h5>
+                            <p class='card-text'>${movie.opening_crawl}</p>
+                            <p class='card-text'>Director: ${movie.director}</p>
+                            <p class='card-text'>Producer ${movie.producer}</p>
+                            <p class='card-text'>Release date: ${movie.release_date}</p>
+                        </div>
+                    </div>
+                </div>`;
             })
         })
         .catch(function (error) {
             console.log(error)
-        })
+        });
 }
